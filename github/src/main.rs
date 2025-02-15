3e429b65d67d33f28122cfefa058930951e8d037
Jason Son <jasonson2004@gmail.com>
2025-02-15 13:59:24 -0800

    Web server can now take in webhook POSTs

--- github/src/main.rs
+++ github/src/main.rs
@@ -1,15 +1,19 @@
 // main.rs
-use axum::Router;
-use axum::response::IntoResponse;
-use axum::routing::post;
+use axum::{Router, response::IntoResponse, routing::post, extract::Json};
 use reqwest::Client;
-use serde_json::Value;
+// use serde_json::Value;
+// use serde_json::json;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use hmac::{Hmac, Mac};
+use sha2::Sha256;
+use hex;
+
 
 pub async fn serve() {
     let client = Arc::new(Client::new());
-    let app = Router::new().route("/analyze_event", post(analyze_handler));
+    let app = Router::new()
+        .route("/webhook", post(handle_github_webhook));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!(
@@ -25,4 +29,10 @@ pub async fn serve() {
 #[tokio::main]
 async fn main() {
     serve().await;
+}
+
+// handling github webhook
+async fn handle_github_webhook(Json(payload): Json<serde_json::Value>) -> impl IntoResponse {
+    println!("Received webhook: {:?}", payload);
+    println!("End of payload");
 }
